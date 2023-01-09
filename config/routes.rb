9e8bf5291b20f544daafbdc4133e644f620df685
Clement Avenel <clement@squadracer.com>
2023-01-09 01:15:54 +0100

    Default format JSON

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :friends
     end
