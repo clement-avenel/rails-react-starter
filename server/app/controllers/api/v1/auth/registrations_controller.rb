2f9674789de158e48bc5d958cd778e5c7496248c
Clement Avenel <clement@squadracer.com>
2023-01-16 02:44:09 +0100

    Lint JSON renderers

--- server/app/controllers/api/v1/auth/registrations_controller.rb
+++ server/app/controllers/api/v1/auth/registrations_controller.rb
@@ -7,14 +7,20 @@ class Api::V1::Auth::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
-      render json: {
-        status: {code: 200, message: 'Signed up sucessfully.'},
-        data: resource
-      }, status: :ok
+      render(
+        json: {
+          status: { code: 200, message: 'Signed up sucessfully.' },
+          data: resource
+        },
+        status: :ok
+      )
     else
-      render json: {
-        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
-      }, status: :unprocessable_entity
+      render(
+        json: {
+          status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+        },
+        status: :unprocessable_entity
+      )
     end
   end
 
--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < ApplicationController
-    before_action :authenticate_user!
-    def index
-      render json: {
+  before_action :authenticate_user!
+  def index
+    render(
+      json: {
         status: 200,
-        message: "Valid session"
+        message: 'Valid session'
       }, status: :ok
-    end
+    )
+  end
 end
