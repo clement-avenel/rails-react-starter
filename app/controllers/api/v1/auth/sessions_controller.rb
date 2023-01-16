081831bc22e6373457584482561ea143b21089ef
Clement Avenel <clement@squadracer.com>
2023-01-16 02:44:09 +0100

    Lint JSON renderers

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -6,23 +6,29 @@ class Api::V1::Auth::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: {
-      status: {code: 200, message: 'Logged in sucessfully.'},
-      data: resource
-    }, status: :ok
+    render(
+      json: {
+        status: { code: 200, message: 'Logged in sucessfully.' },
+        data: resource
+      }, status: :ok
+    )
   end
 
   def respond_to_on_destroy
     if current_user
-      render json: {
-        status: 200,
-        message: "Logged out successfully"
-      }, status: :ok
+      render(
+        json: {
+          status: 200,
+          message: 'Logged out successfully'
+        }, status: :ok
+      )
     else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+      render(
+        json: {
+          status: 401,
+          message: "Couldn't find an active session."
+        }, status: :unauthorized
+      )
     end
   end
 
