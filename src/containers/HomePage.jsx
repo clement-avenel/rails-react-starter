8a943a292de67eef29213261ccfff241bbec26df
Clement Avenel <clement@squadracer.com>
2023-01-18 01:38:28 +0100

    Add Chart.js

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,21 +1,92 @@
 import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut, Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const pieData = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+};
+
+const doughnutData = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+};
 
 function HomePage() {
   return (
-    <div className="w-full block p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">
-        Hello{' '}
-        <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">
-          John Doe
-        </span>{' '}
-        👋
-      </h1>
-      <p className="text-md font-normal text-gray-500 lg:text-md dark:text-gray-400">
-        This is a starter template for a Rails & React application. It combines the power of the
-        React JavaScript framework with the simplicity and robustness of the Rails web application
-        framework. The project is set up with a Rails backend that serves a JSON API and a React
-        frontend that consumes the API and manages the application&apos;s state.
-      </p>
+    <div className="gap-2">
+      <div className="w-full block p-6 mb-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">
+          Hello{' '}
+          <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">
+            John Doe
+          </span>{' '}
+          👋
+        </h1>
+        <p className="text-md font-normal text-gray-500 lg:text-md dark:text-gray-400">
+          This is a starter template for a Rails & React application. It combines the power of the
+          React JavaScript framework with the simplicity and robustness of the Rails web application
+          framework. The project is set up with a Rails backend that serves a JSON API and a React
+          frontend that consumes the API and manages the application&apos;s state.
+        </p>
+      </div>
+      <div className="flex space-x-6">
+        <div className="w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <Pie data={pieData} />
+        </div>
+        <div className="w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <Doughnut data={doughnutData} />
+        </div>
+        <div className="w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <Pie data={pieData} />
+        </div>
+      </div>
     </div>
   );
 }
