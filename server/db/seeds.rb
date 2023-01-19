56341605e2a0ffd13b85f1162df40ea1a37a1fc5
Clement Avenel <clement@squadracer.com>
2023-01-19 20:25:04 +0100

    Add Procfile & update db seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -3,6 +3,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+User.create(
+  [
+    {
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john@doe.fr',
+      password: 'password',
+    }
+    }
+  ]
+)
 Friend.create([
   {
     name: 'Anna Smith',
